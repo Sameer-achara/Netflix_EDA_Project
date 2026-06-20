5d8a93912453a79212b4bce859d255175f89a860
sameer-achara <sameerachara894@gmail.com>
2026-06-20 11:40:08 +0530

    Updated project

--- sam.py
+++ sam.py
@@ -17,11 +17,13 @@ import streamlit as st
 # 11. Box Plot (Ratings Distribution)
 
 st.set_page_config(page_title="Netflix Content Analytics", page_icon="C:\\Users\\DELL\\Downloads\\netflix.png", layout="wide")
-st.markdown("<h1 style='text-align:center; color: #60A5FA;'>🌤️ Netflix Content Analytics Portal</h1>",unsafe_allow_html=True,)
+st.markdown("<h1 style='text-align:center; color: #60A5FA;'>Netflix Content Analytics Portal</h1>",unsafe_allow_html=True,)
 st.markdown("<p style='text-align:center; color: #FACC15;'>Advanced Trends Detection, Content Correlation Study, and Movies/Show Insights</p>",unsafe_allow_html=True,)
 st.markdown("---")
 
 uploaded_file = st.file_uploader("Please Upload CSV File",type=["CSV"])
 if uploaded_file:
     df=pd.read_csv(uploaded_file)
-    st.dataframe(df)
+    st.write(df.isnull().sum())
+    st.write("total duplicated raws:",df.duplicated().sum())
+    st.write(df[df.duplicated()])
